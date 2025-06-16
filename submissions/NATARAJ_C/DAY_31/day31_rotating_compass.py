7cf25344e72fe7c861f60b8a7092796d0ffbbb17
Nataraj-chandrasekaran <23l134@psgtech.ac.in>
2025-06-16 23:04:04 +0530

    Update day31_rotating_compass.py

--- submissions/NATARAJ_C/DAY_31/day31_rotating_compass.py
+++ submissions/NATARAJ_C/DAY_31/day31_rotating_compass.py
@@ -2,4 +2,4 @@ list1=list(map(int,input("Enter numbers: ").split()))
 k=int(input("Enter rotation value(k): "))
 k=k%len(list1)
 rotated_list=list1[k:]+list1[:k]     
-print("Rotated list:",*rotated_list)           
+print("Rotated list:",*rotated_list)            
